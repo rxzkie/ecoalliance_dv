3666e68bc43610e2c9aab6ca8ffe475bac6881ae
rxzkie <54205705+rxzkie@users.noreply.github.com>
2024-11-07 17:46:03 -0300

    asd
    asd

--- ecoalliancedj/ecoalliancedj/settings.py
+++ ecoalliancedj/ecoalliancedj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^xrn3!6^a89v$my=&iy_c8f&w$uj-hnag5klben!#84i4)8^9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecoalliance.pythonanywhere.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -47,10 +47,7 @@ INSTALLED_APPS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-        
-
-        "https://ecoalliance.pythonanywhere.com"
-
+    "http://localhost:8080",
 ]
 
 
@@ -135,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
