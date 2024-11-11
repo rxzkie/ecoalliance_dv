5b96e8ad7d72b33cdfcb2cb38d0baa6011b657a1
rxzkie <54205705+rxzkie@users.noreply.github.com>
2024-11-11 11:20:29 -0300

    asd
    asd

--- ecoalliancedj/productos/models.py
+++ ecoalliancedj/productos/models.py
@@ -1,66 +1,66 @@
 from io import BytesIO
 from PIL import Image
-
 from django.core.files import File
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('nombre',)
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
     
     def __str__(self):
-        return self.name
+        return self.nombre
     
     def get_absolute_url(self):
         return f'/{self.slug}/'
 
-class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+class Productos(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='productos', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    miniatura = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    fecha_agregado = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('-date_added',)
+        ordering = ('-fecha_agregado',)
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
     
     def __str__(self):
-        return self.name
+        return self.nombre
     
     def get_absolute_url(self):
-        return f'/{self.category.slug}/{self.slug}/'
+        return f'/{self.categoria.slug}/{self.slug}/'
     
-    def get_image(self):
-        if self.image:
-            return 'http://127.0.0.1:8000' + self.image.url
+    def get_imagen(self):
+        if self.imagen:
+            return self.imagen.url
         return ''
     
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return 'http://127.0.0.1:8000' + self.thumbnail.url
-        else:
-            if self.image:
-                self.thumbnail = self.make_thumbnail(self.image)
-                self.save()
-
-                return 'http://127.0.0.1:8000' + self.thumbnail.url
-            else:
-                return ''
+    def get_miniatura(self):
+        if self.miniatura:
+            return self.miniatura.url
+        elif self.imagen:
+            self.miniatura = self.crear_miniatura(self.imagen)
+            self.save()
+            return self.miniatura.url
+        return ''
     
-    def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
+    def crear_miniatura(self, imagen, tamaño=(300, 200)):
+        img = Image.open(imagen)
         img.convert('RGB')
-        img.thumbnail(size)
+        img.thumbnail(tamaño)
 
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
 
-        thumbnail = File(thumb_io, name=image.name)
+        miniatura = File(thumb_io, name=imagen.name)
 
-        return thumbnail
+        return miniatura
