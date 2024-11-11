f5859895bb61d68ed553f7f131d0a14a0dfde7e9
rxzkie <54205705+rxzkie@users.noreply.github.com>
2024-11-11 12:35:47 -0300

    asd
    asd

--- ecoalliancedj/ecoalliancedj/settings.py
+++ ecoalliancedj/ecoalliancedj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^xrn3!6^a89v$my=&iy_c8f&w$uj-hnag5klben!#84i4)8^9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ecoalliance.pythonanywhere.com']
+ALLOWED_HOSTS = ['ecoalliance.pythonanywhere.com']
 
 
 # Application definition
